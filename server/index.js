8354eb44c46afc1dc3a7f83b938c030e49555181
ae0nic <bzminecraft247@gmail.com>
2024-05-17 14:40:27 -0500

    Completely revamp base math question generation

--- server/index.js
+++ server/index.js
@@ -7,6 +7,44 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+function generateBaseMathQuestion()
+{
+  let questionBases = [[2, "₂"], [4, "₄"], [8, "₈"], [16, "₁₆"]];
+  let answerBases = [[2, "₂"], [4, "₄"], [8, "₈"], [10, "₁₀"], [16, "₁₆"]];
+  let operators = [["*", (num1, num2) => num1*num2], ["+", (num1, num2) => num1+num2]];
+
+  let question = {};
+
+  let num1 = Math.floor(Math.random() * 100 + 30);
+  let [base1Num, base1Subscript] = questionBases[Math.floor(Math.random() * questionBases.length)];
+  let [base2Num, base2Subscript] = questionBases[Math.floor(Math.random() * questionBases.length)];
+  let num2 = Math.floor(Math.random() * 100 + 30);
+  let [operatorText, operatorLambda] = operators[Math.floor(Math.random() * operators.length)];
+
+  
+  
+  let answer = operatorLambda(num1, num2);
+  let [baseAnswerNum, baseAnswerSubscript] = answerBases[Math.floor(Math.random() * answerBases.length)];
+  question["Question"] = 
+  `Which of the following is equal to ${num1.toString(base1Num) + base1Subscript}  ${operatorText} ${num2.toString(base2Num) + base2Subscript}?`;
+  
+  question["CorrectChoice"] = Math.floor(Math.random() * 4);
+  question["Choices"] = {};
+
+  for (let i = 0; i < 4; i++)
+  {
+    let sign = Math.sign(Math.random() - 0.5) || 1;
+    question["Choices"][i] = (Math.floor(Math.random() * 20 + 20 * sign) + answer)
+      .toString(baseAnswerNum)
+      .toUpperCase() + baseAnswerSubscript;
+  }
+
+  question["Choices"][question["CorrectChoice"]] = answer
+    .toString(baseAnswerNum)
+    .toUpperCase() + baseAnswerSubscript;
+  return question;
+}
+
 function generateResponse(questions)
 {
   let toReturn = {};
@@ -20,26 +58,13 @@ function generateResponse(questions)
       for (let i = 1; i <= numQuestions; i++)
       {
         
-        data[i] = {};
-        let num1 = Math.floor(Math.random() * 20 + 20);
-        let num2 =  Math.floor(Math.random() * 20 + 20);
-        let operator = (Math.random() < 0.5) ? " sum " : " product ";
-
-        let answer = (operator === " sum ") ? num1 + num2 : num1 * num2;
-        data[i]["Question"] = `What is the ${operator} of ${num1.toString(16)}₁₆ and ${num2.toString(16)}₁₆?`;
-        data[i]["CorrectChoice"] = Math.floor(Math.random() * 4);
-        data[i]["Choices"] = {};
-        data[i]["Choices"][0] = data[i]["Answer"] = Math.floor(Math.random() * 40 + 10) + "₁₀";
-        data[i]["Choices"][1] = data[i]["Answer"] = Math.floor(Math.random() * 40 + 10) + "₁₀";
-        data[i]["Choices"][2] = data[i]["Answer"] = Math.floor(Math.random() * 40 + 10) + "₁₀";
-        data[i]["Choices"][3] = data[i]["Answer"] = Math.floor(Math.random() * 40 + 10) + "₁₀";
-
-        data[i]["Choices"][data[i]["CorrectChoice"]] = answer + "₁₀";
+        data[i] = generateBaseMathQuestion();
         
       }
       toReturn["data"] = data;
     }
-  } catch {
+  } catch (e) {
+    console.log(e);
     toReturn["data"] = {"message": "The server threw an error!"}
   } finally {
     return toReturn;
