738c333e5ae3f7675ec235abb804ae7efc57323b
ae0nic <bzminecraft247@gmail.com>
2024-07-24 19:09:01 -0500

    Small change to proxy (for futureproofing)

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,9 +3,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     console.log("Creating proxy middleware")
     app.use(
-        '/api/generateQuestions',
+        '/api',
         createProxyMiddleware({
-        target: 'http://127.0.0.1:3001/api/generateQuestions',
+        target: 'http://127.0.0.1:3001/api',
         changeOrigin: true,
         })
     );
