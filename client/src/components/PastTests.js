f337c440770130b35261ca4849e0e0f6ce37c9ba
ae0nic <bzminecraft247@gmail.com>
2024-07-25 17:51:32 -0500

    Tests test
    Whoops forgot to delete some boilerplate

--- client/src/components/PastTests.js
+++ client/src/components/PastTests.js
@@ -1,12 +1,15 @@
+import { useEffect } from 'react';
 import './PastTests.scss';
 
 export default function PastTests() {
+    useEffect(() => {
+        // call the api
+    });
     return (<div id="body">
         <div id="body-header">
             View Past UIL Tests
         </div>
         <hr/>
         <h3>Tests here!</h3>
-        
     </div>);
 }
